e94db146e6d6320320762a0cf9d5326e43dd7acf
vahiDevi <va.mohammadi@digikala.com>
2025-12-29 21:36:16 +0330

    mark task as complete

--- cmd/complete.go
+++ cmd/complete.go
@@ -1,1 +1,48 @@
 package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/devi-vahid/command-line-todo/internal/task"
+	"github.com/spf13/cobra"
+)
+
+var completeCmd = &cobra.Command{
+	Use:   "complete [id]",
+	Short: "mark task as completed",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		taskId, err := strconv.Atoi(args[0])
+
+		if nil != err {
+			return err
+		}
+
+		tasks, err := task.LoadTasks(dataFile)
+
+		if nil != err {
+			return err
+		}
+
+		for i := 0; i < len(tasks); i++ {
+			if taskId == tasks[i].ID {
+				tasks[i].IsComplete = true
+			}
+		}
+
+		err = task.SaveTasks(dataFile, tasks)
+
+		if nil != err {
+			return err
+		}
+
+		fmt.Fprintf(cmd.OutOrStdout(), "Marked task as complete: %s\n", taskId)
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completeCmd)
+}
